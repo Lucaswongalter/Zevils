b0f16d6e84aebada13af2a8c6f02414407128d1d
Matthew Sachs <matthewg@zevils.com>
2008-03-16 04:32:04 +0000

    Checkpoint my work

--- wedweb/header.js
+++ wedweb/header.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+    //Email address obfuscation adapted from http://w2.syronex.com/jmr/safemailto/
+    var v2 = 'BVQH3ARCMT6YBV8';
+    var v7 = unescape('7%25%11%3BR%22%3A0+5%5Bw-%24_');
+    var v5 = v2.length;
+    var v1 = '';
+    for(var v4=0; v4 < v5; v4++) {
+	v1 += String.fromCharCode(v2.charCodeAt(v4)^v7.charCodeAt(v4));
+    }
+    $("#footer-email").text("");
+    $("#footer-email").append('<a href="javascript:void(0)">us -AT- sachsfam -DOT- org</a>');
+    $("#footer-email > a").click(function() {
+	window.location="mail\u0074o\u003a" + v1;
+    });
+
     $("dl.glidey > dd:not(:first)").hide();
     $("dl.glidey > dt").click(function(){
         $("dd:visible").slideUp();
