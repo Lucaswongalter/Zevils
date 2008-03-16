082a6184ec4d894353a2729d13ed24ad9698ba1c
Matthew Sachs <matthewg@zevils.com>
2008-03-16 04:26:39 +0000

    Checkpoint Liz's work

--- wedweb/about.php
+++ wedweb/about.php
@@ -4,9 +4,7 @@
 <dt><span class="header-text all-header-text">How We Met</span></dt>
 <dd>
 <p>
-Lorem ipsum dolor sic amit.  Spans are defined for <span class="time">4:30 p.m.</span>
-and other such times, <span class="date">Sunday, October 12<sup>th</sup>, 2008</span> and similar dates,
-and <span class="location">Waltham, Massachusetts</span>, &amp;c.
+We were both undergraduate students in the computer science department at Brandeis, but in a valiant attempt to break the tradition on both sides of the family of meeting one's spouse in school, we almost managed to finish our collegiate careers without speaking to one another.
 </p>
 
 <h3><span class="subheader-text all-header-text">Volun tatis pecatta mundi</span></h3>
@@ -16,21 +14,14 @@ and <span class="location">Waltham, Massachusetts</span>, &amp;c.
 
 <dt><span class="header-text all-header-text">Liz</span></dt>
 <dd>
-<p><em>Hotel rooms in Boston fill up early.  We recommend reserving rooms as early as possible.</em></p>
+<p>Liz was born in Silicon Valley, and lived with her parents for a short time in California and Minnesota  before settling in Texas for about 15 years.  Despite the lengthy duration in Texas, she still harbors love for technology and snow that undoubtably stem from the brief stints in other states.  She did acquire a great love for Tex-Mex over the years, but never managed to pick up a Southern accent, so you probably wouldn't guess where she grew up from talking to her!</p>
 
-<h3><span class="subheader-text all-header-text">Hyatt Harborside</span></h3>
-<p>The <a href="http://harborside.hyatt.com/">Hyatt Harborside</a>, the location for the ceremony and reception,
-has rooms available at a group rate of <span class="price">$189.00</span> per night for the nights of the
-11<sup>th</sup> and 12<sup>th</sup>; to book a room for this rate, ....</p>
+<p>After a high school career filled with marching band, competitive Lincoln-Douglas debate and Science Olympiad (with a class or two here and there), she headed back north for college at Brandeis University.  There, she majored in computer science while also being active in the Neuroscience department by taking several classes and working in a lab.  She loved neuroscience, artificial intellignce, and human-computer interaction, and strived to combine her interest in all of these fields with a 5th year at Brandeis spent earning her Master's.  While at Brandeis, she also spent several years studying the Japanese language, and gaining an appreciation for Japanese culture.  After graduating in May 2007, she accepted a position as a software engineer at Unica, inc.</p>
 
-<h3><span class="subheader-text all-header-text">Doubletree Inn Waltham</span></h3>
-<p>The <a href="#">Doubletree Inn</a> in Waltham has rooms available at a group rate of <span class="price">$139.00</span>
-per night; to book a room for this rate, ....  There is no shuttle service between the Doubletree and Logan Airport.
-It's about a half-hour drive from the airport.</p>
+<p>Hobbies include the computer (even at home), cooking and a huge amount of knitting.  She adopted a black kitten named Tabitha (Tabi, to us) in August 2006, who never fails to keep life interesting at home.  Luckily for everyone, Tabi seems to think Matthew is one of the greatest people ever!</p>
 
-<h3><span class="subheader-text all-header-text">Hilton Boston Logan Airport</span></h3>
-<p>The <a href="#">Hilton Boston Logan Airport</a> has rooms available at a group rate of <span class="price">$239.00</span>
-per night; to book a room for this rate, ....  The hotel is connected to the airport terminals via a walkway.</p>
+<p>You can catch up with Liz's latest knitting projects and other happenings on her blog: <a href="http://blog.blacktabi.com/">BlackTabi Blog</a>  </p>
+
 </dd>
 
 
@@ -55,7 +46,12 @@ The quick brown fox jumped over the lazy dogs.
 <dt><span class="header-text all-header-text">Sachs Family</span></dt>
 <dd>
 <p>
-Now is the time for all good men to come to the aid of their party.
+Matthew&rsquo;s proud parents are Randi and Danny Sachs and his equally proud brothers are Evan and Jeffrey. Randi and Danny met in college at the State University of New York (SUNY) at Albany when Randi was just a freshman and Danny was a senior. They met at a party and soon became inseparable.  Four years later shortly after Randi graduated from NYU (where she had transferred to be closer to Danny and also to study journalism) they were married. The date was August 19, 1978.</p>
+<p>Danny began his career coding computer programs. His first job was for R.H. Macy&rsquo;s in Herald Square where we took part in the Thanksgiving Day Parade dressed as clowns, and next he worked for the now-defunct Manufacturer&rsquo;s Hanover. He currently works as a computer manager for State Street, which is headquartered in Boston, but his office is in downtown Manhattan. After a few false starts, including a brief stint as an editor at &ldquo;Master Detective&rdquo; magazine, Randi began a writing and editorial career in trade journalism, specifically in the management field. Matthew&rsquo;s arrival began a twelve-year period at home with the kids, where she continued writing management material, Randi then went back into the workforce full time as a public relations officer. She is currently Communications Manager at Whitsons Culinary Group, a food service organization, and has found an exception to the old saying that &ldquo;there is no such thing as a free lunch.&rdquo;</p>
+<p>After about four years of marriage, their friends starting having babies. It seemed like an epidemic. We figured, as long as we&rsquo;re going to be around babies so much, we might as well have one of our own. Randi&rsquo;s goal was to have &ldquo;a little Danny,&rdquo; since she had missed knowing him in childhood, and miracle of miracles, on March 20, 1984 a son was born. Matthew Gabriel Sachs was named after grandparents Max and Mickey and Goldie and Gertrude. He&rsquo;s the groom, so you can read about him in his own section.</p>
+<p>Randi and Danny will be celebrating their 30th anniversary in August and are thrilled to be finally adding a daughter (Liz) after raising three wonderful sons. (Check out our license plate, it reads BOYSRUS3.) We live in Merrick (Long Island), NY. Middle son Evan, 20, is currently a junior at SUNY Albany (sound familiar?) and is studying psychology and math. Youngest son Jeffrey, 16 going on 17, is a junior in high school and is greatly involved in an intensive four-year drama program. He recently had his first leading role in a musical and he was, well, magnificent. He hopes to continue to study and get a degree in theater, but he has promised to complete a double major to have the academic degree to fall back on &ldquo;just in case.&rdquo;</p>
+<p>The Sachs family could not be complete without a mention of our two cats, Baby and Surge. Baby, a black and white behemoth is a loving and friendly fellow who seems to have the sort of metabolism that likes to hold onto calories. Surge is a long-haired orange cat with a propensity for biting people on the leg with razor-sharp teeth. We also have a pair of leopard geckos, who don&rsquo;t do much of anything but hang out in their tank.</p>
+<p>We are all very excited about Matthew and Elizabeth&rsquo;s wedding. Liz fits very comfortably into our family and most of all, we have never known Matthew to be happier. It&rsquo;s a perfect match!
 </p>
 </dd>
 
---
+++ wedweb/photos.php
@@ -0,0 +1,45 @@
+<? include("header.inc"); ?>
+<dl class="fancy-images">
+
+<dt><span class="header-text all-header-text">Liz and Matthew</span></dt>
+<dd>
+<p>
+<? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
+<? fancyImage("us.png", "The Sachs Gang circa 1996.", 200, 150); ?>
+<? fancyImage("us.png", "On the streets of San Francisco, visiting Matthew during his Apple internship.", 200, 150); ?>
+<? fancyImage("us.png", "Surge and Baby in their cat condo.", 200, 150); ?>
+<? fancyImage("us.png", "Matthew graduates from Brandeis, grandmothers Dotty and Zelda join the celebration.", 200, 150); ?>
+</p></dd>
+
+<dt><span class="header-text all-header-text">Tabi</span></dt>
+<dd>
+<p>
+<? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
+<? fancyImage("us.png", "The Sachs Gang circa 1996.", 200, 150); ?>
+<? fancyImage("us.png", "On the streets of San Francisco, visiting Matthew during his Apple internship.", 200, 150); ?>
+<? fancyImage("us.png", "Surge and Baby in their cat condo.", 200, 150); ?>
+<? fancyImage("us.png", "Matthew graduates from Brandeis, grandmothers Dotty and Zelda join the celebration.", 200, 150); ?>
+</p></dd>
+
+<dt><span class="header-text all-header-text">Gifford Family</span></dt>
+<dd>
+<p>
+<? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
+<? fancyImage("us.png", "The Sachs Gang circa 1996.", 200, 150); ?>
+<? fancyImage("us.png", "On the streets of San Francisco, visiting Matthew during his Apple internship.", 200, 150); ?>
+<? fancyImage("us.png", "Surge and Baby in their cat condo.", 200, 150); ?>
+<? fancyImage("us.png", "Matthew graduates from Brandeis, grandmothers Dotty and Zelda join the celebration.", 200, 150); ?>
+</p></dd>
+
+<dt><span class="header-text all-header-text">Sachs Family</span></dt>
+<dd>
+<p>
+<? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
+<? fancyImage("sachsgang.jpg", "The Sachs Gang circa 1996.", 200, 150); ?>
+<? fancyImage("sachINscity.jpg", "On the streets of San Francisco, visiting Matthew during his Apple internship.", 200, 150); ?>
+<? fancyImage("catcondo1.JPG", "Surge and Baby in their cat condo.", 200, 150); ?>
+<? fancyImage("familygrad.JPG", "Matthew graduates from Brandeis, grandmothers Dotty and Zelda join the celebration.", 200, 150); ?>
+</p></dd>
+
+</dl>
+<? include("footer.inc"); ?>
