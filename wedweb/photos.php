dd0d9d9ca0df57d9ea0d0546d054c4feac7081c7
Matthew Sachs <matthewg@zevils.com>
2008-03-17 03:21:35 +0000

    Fix thumbnail sizes

--- wedweb/photos.php
+++ wedweb/photos.php
@@ -6,49 +6,49 @@
 <dd>
 <p>
 <? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
-<? fancyImage("tksgiving06.jpg", "Matthew and Liz - Thanksgiving 2006.", 142, 200); ?>
-<? fancyImage("mattliztksgiving07.jpg", "Matthew and Liz - Thanksgiving 2007.", 200, 133); ?>
-<? fancyImage("mattliztree.jpg", "Rockefeller Center - December 2007.", 150, 200); ?>
-<? fancyImage("waffle.jpg", "Matthew eagerly anticipates digging into his 'Multigrain Delight.'", 150, 200); ?>
-<? fancyImage("bento.jpg", "Liz displays a bento boxed lunch, complete with monkey containers to hold dressing.", 150, 200); ?>
-<? fancyImage("mattSAzoo.jpg", "Matthew communes with the lorikeets at the San Antonio Zoo.", 150, 200); ?>
-<? fancyImage("mattMacs.jpg", "Matthew with the 'Mac' apples on a New England apple-picking excursion.", 150, 200); ?>
-<? fancyImage("appleButter.jpg", "Liz pulls a batch of apple butter out of the canner.", 150, 200); ?>
-<? fancyImage("rings.jpg", "We bought each other coordinating engagement rings!", 200, 133); ?>
+<? fancyImage("tksgiving06.jpg", "Matthew and Liz - Thanksgiving 2006.", 91, 128); ?>
+<? fancyImage("mattliztksgiving07.jpg", "Matthew and Liz - Thanksgiving 2007.", 128, 85); ?>
+<? fancyImage("mattliztree.jpg", "Rockefeller Center - December 2007.", 96, 128); ?>
+<? fancyImage("waffle.jpg", "Matthew eagerly anticipates digging into his 'Multigrain Delight.'", 96, 128); ?>
+<? fancyImage("bento.jpg", "Liz displays a bento boxed lunch, complete with monkey containers to hold dressing.", 96, 128); ?>
+<? fancyImage("mattSAzoo.jpg", "Matthew communes with the lorikeets at the San Antonio Zoo.", 96, 128); ?>
+<? fancyImage("mattMacs.jpg", "Matthew with the 'Mac' apples on a New England apple-picking excursion.", 96, 128); ?>
+<? fancyImage("appleButter.jpg", "Liz pulls a batch of apple butter out of the canner.", 128, 96); ?>
+<? fancyImage("rings.jpg", "We bought each other coordinating engagement rings!", 128, 85); ?>
 </p></dd>
 
 <dt class="bar-header"><span class="header-text all-header-text">Tabi</span></dt>
 <dd>
 <p>
 <? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
-<? fancyImage("tabitree.jpg", "Tabi looks down from her tower.", 200, 150); ?>
-<? fancyImage("tabibed.jpg", "Tabi with one of her (many) toys.", 200, 150); ?>
-<? fancyImage("tabifloor.jpg", "Tabi finds a sunspot.", 200, 150); ?>
+<? fancyImage("tabitree.jpg", "Tabi looks down from her tower.", 128, 96); ?>
+<? fancyImage("tabibed.jpg", "Tabi with one of her (many) toys.", 128, 96); ?>
+<? fancyImage("tabifloor.jpg", "Tabi finds a sunspot.", 128, 96); ?>
 </p></dd>
 
 <dt class="bar-header"><span class="header-text all-header-text">Gifford Family</span></dt>
 <dd>
 <p>
 <? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
-<? fancyImage("lizgraduation.jpg", "Liz's undergrad commencement (2006): Dave (dad), Alexander (brother), Liz, Jean (mom), Isaac (brother), Marilynne (grandmother), Paul (grandfather).", 200, 150); ?>
-<? fancyImage("giffords_orlando.jpg", "The Giffords visit Grandma and Grandpa in Orlando (2002): (back row) Russ (grandfather), Alex (brother), Dave (dad), Jean (mom); (front row) Liz, Joan (grandmother), Isaac (brother).", 200, 150); ?>
-<? fancyImage("totem.jpg", "Dave, Jean and Isaac mimicing the totem poles in Victoria, BC.", 200, 150); ?>
-<? fancyImage("gandgGifford.jpg", "Russ and Joan celebrating Joan's birthday.", 200, 150); ?>
-<? fancyImage("AlexandersCoffee.jpg", "Paul and Marilynne enjoy coffee in the North End of Boston while visiting Liz in 2004.", 200, 150); ?>
-<? fancyImage("GcatsTower.jpg", "Sushi and Pandora grudgingly share their cat condo.", 200, 150); ?>
-<? fancyImage("mauandmoosh.jpg", "Pandora and Sushi claim the human furniture.", 200, 150); ?>
-<? fancyImage("birdbath.jpg", "Giffords visit Grandma and Grandpa in Colorado.", 200, 150); ?>
-<? fancyImage("GiffordsEarly1.jpg", "Dressed up for Easter church.", 200, 150); ?>
+<? fancyImage("lizgraduation.jpg", "Liz's undergrad commencement (2006): Dave (dad), Alexander (brother), Liz, Jean (mom), Isaac (brother), Marilynne (grandmother), Paul (grandfather).", 128, 96); ?>
+<? fancyImage("giffords_orlando.jpg", "The Giffords visit Grandma and Grandpa in Orlando (2002): (back row) Russ (grandfather), Alex (brother), Dave (dad), Jean (mom); (front row) Liz, Joan (grandmother), Isaac (brother).", 128, 100); ?>
+<? fancyImage("totem.jpg", "Dave, Jean and Isaac mimicing the totem poles in Victoria, BC.", 96, 128); ?>
+<? fancyImage("gandgGifford.jpg", "Russ and Joan celebrating Joan's birthday.", 128, 101); ?>
+<? fancyImage("AlexandersCoffee.jpg", "Paul and Marilynne enjoy coffee in the North End of Boston while visiting Liz in 2004.", 128, 96); ?>
+<? fancyImage("GcatsTower.jpg", "Sushi and Pandora grudgingly share their cat condo.", 96, 128); ?>
+<? fancyImage("mauandmoosh.jpg", "Pandora and Sushi claim the human furniture.", 128, 96); ?>
+<? fancyImage("birdbath.jpg", "Giffords visit Grandma and Grandpa in Colorado.", 128, 89); ?>
+<? fancyImage("GiffordsEarly1.jpg", "Dressed up for Easter church.", 128, 89); ?>
 </p></dd>
 
 <dt class="bar-header"><span class="header-text all-header-text">Sachs Family</span></dt>
 <dd>
 <p>
 <? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
-<? fancyImage("sachsgang.jpg", "The Sachs Gang circa 1996.", 180, 200); ?>
-<? fancyImage("sachINscity.jpg", "On the streets of San Francisco, visiting Matthew during his Apple internship.", 200, 131); ?>
-<? fancyImage("catcondo1.jpg", "Surge and Baby in their cat condo.", 200, 150); ?>
-<? fancyImage("familygrad.jpg", "Matthew graduates from Brandeis, grandmothers Dotty and Zelda join the celebration.", 200, 150); ?>
+<? fancyImage("sachsgang.jpg", "The Sachs Gang circa 1996.", 115, 128); ?>
+<? fancyImage("sachINscity.jpg", "On the streets of San Francisco, visiting Matthew during his Apple internship.", 128, 84); ?>
+<? fancyImage("catcondo1.jpg", "Surge and Baby in their cat condo.", 128, 96); ?>
+<? fancyImage("familygrad.jpg", "Matthew graduates from Brandeis, grandmothers Dotty and Zelda join the celebration.", 128, 96); ?>
 </p></dd>
 
 </dl>
