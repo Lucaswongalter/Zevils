6aaebe4fa3d46550146148b725c4877ecd8611ac
Matthew Sachs <matthewg@zevils.com>
2008-03-17 23:39:31 +0000

    Checkpoint Liz

--- wedweb/index.php
+++ wedweb/index.php
@@ -5,7 +5,7 @@
 <dd>
 <p>
 <? /* fancyImage("filename.jpg", "The Caption Goes Here", thumbnail-width, thumbnail-height); */ ?>
-<? fancyImage("us.png", "Matthew and Liz - Thanksgiving 2006.", 200, 150); ?>
+<? fancyImage("us.png", "Matthew, Liz and Tabi.", 200, 150); ?>
 </p></dd>
 
 <? include("footer.inc"); ?>
