9f8bfdd3938b426dfd4cad5126199f95e62d6beb
Matthew Sachs <matthewg@zevils.com>
2008-08-10 20:37:41 +0000

    Parens are your friend

--- wedweb/rsvp_server.php
+++ wedweb/rsvp_server.php
@@ -292,12 +292,13 @@ function authenticate_guest($path = array()) {
     $norm_street_name = strtoupper($street_name);
     $norm_street_name = preg_replace("/ (STREET|ST|PLACE|PL|AVE|AVENUE|DRIVE|DR|COURT|CT|EXPRESSWAY|EXPY|PARKWAY|PKWY|TURNPIKE|TPKE)\\.?$/", "", $norm_street_name);
     
-    $sql = sprintf("SELECT * FROM groups WHERE street_name='%s' OR street_name LIKE '%%%s'",
+    $sql = sprintf("SELECT * FROM groups WHERE (street_name='%s' OR street_name LIKE '%%%s')",
                    mysql_real_escape_string($norm_street_name),
                    mysql_real_escape_string($norm_street_name));
     if($group_id) {
       $sql .= sprintf(" AND group_id=%d", $group_id);
     }
+    set_ret("sql", $sql);
     $candidates = sql_fetch_all_hash($sql);
     $groups = array();
     $group_people = array();
