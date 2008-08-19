9d8c4b00f6c498babeefffd6d293f5928dd1f04b
Matthew Sachs <matthewg@zevils.com>
2008-08-19 00:29:30 +0000

    Booking deadlines

--- wedweb/info.php
+++ wedweb/info.php
@@ -18,16 +18,19 @@ in Boston, Massachusetts.
 <h3><span class="subheader-text all-header-text">Hyatt Harborside</span></h3>
 <p>The <a href="http://harborside.hyatt.com/">Hyatt Harborside</a>, the location for the ceremony and reception,
 has rooms available at a group rate of <span class="price">$189.00</span> per night for the nights of the
-11<sup>th</sup> and 12<sup>th</sup>. The Hyatt is located a few minutes from the airport (it shares a parking lot with the airport "cell phone waiting" lot), and there is a free shuttle from the airport.  To book a room at the Hyatt, call <span class="phone">(800) 233-1234</span> and ask for the Gifford-Sachs room block.</p>
+11<sup>th</sup> and 12<sup>th</sup>. To obtain the group rate, you
+  must make your reservation by September 10<sup>th</sup>.  The Hyatt is located a few minutes from the airport (it shares a parking lot with the airport "cell phone waiting" lot), and there is a free shuttle from the airport.  To book a room at the Hyatt, call <span class="phone">(800) 233-1234</span> and ask for the Gifford-Sachs room block.</p>
 
 <h3><span class="subheader-text all-header-text">Doubletree Inn Waltham</span></h3>
 <p>The <a href="http://doubletree1.hilton.com/en_US/dt/hotel/BOSOWDT-Doubletree-Guest-Suites-Boston-Waltham-Massachusetts/index.do">Doubletree Inn</a> in Waltham has rooms available at a group rate of <span class="price">$139.00</span>
-per night.  There is no shuttle service between
+per night.  To obtain the group rate, you must make your reservation
+  by September 19<sup>th</sup>. There is no shuttle service between
 the Doubletree and Logan Airport, and it is about a half-hour drive from the airport. To book a room at the Doubletree, call <span class="phone">(781) 890-6767</span> and ask for the Gifford-Sachs room block.  </p>
 
 <h3><span class="subheader-text all-header-text">Hilton Boston Logan Airport</span></h3>
 <p>The <a href="http://www1.hilton.com/en_US/hi/hotel/BOSLHHH-Hilton-Boston-Logan-Airport-Massachusetts/index.do">Hilton Boston Logan Airport</a> has rooms available at a group rate of <span class="price">$239.00</span>
-per night. The hotel is connected to the airport terminals via a walkway. To book a room at the Hilton, call <span class="phone">(617) 568-6700</span> and ask for the Gifford-Sachs room block.</p>
+per night. To obtain the group rate, you must make your reservation by
+  September 19<sup>th</sup>. The hotel is connected to the airport terminals via a walkway. To book a room at the Hilton, call <span class="phone">(617) 568-6700</span> and ask for the Gifford-Sachs room block.</p>
 </dd>
 
 <dt id="directions"><span class="header-text all-header-text">Directions</span></dt>
