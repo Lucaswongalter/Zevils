9a03101aca0b23716331fd4232256ac6f0509011
Matthew Sachs <matthewg@zevils.com>
2008-08-24 20:06:30 +0000

    Better normalization for PO boxes

--- wedweb/rsvp_server.php
+++ wedweb/rsvp_server.php
@@ -297,7 +297,7 @@ function authenticate_guest($path = array()) {
     $norm_street_name = preg_replace("/ *\\(.*?\\) */", "", $norm_street_name);
     $norm_street_name = preg_replace("/,.*/", "", $norm_street_name);
     $norm_street_name = preg_replace("/ +(APARTMENT|APT\\.) */", "", $norm_street_name);
-    $norm_street_name = preg_replace("/(?!BOX) +#[0-9]+ */", "", $norm_street_name);
+    $norm_street_name = preg_replace("/(?<!BOX) +#[0-9]+ */", "", $norm_street_name);
     $norm_street_name = preg_replace("/ +[A-Z][0-9]*\$/", "", $norm_street_name);
     $norm_street_name = preg_replace(array("/\\bNORTH\\b/",
                                            "/\\bSOUTH\\b/",
@@ -311,7 +311,7 @@ function authenticate_guest($path = array()) {
     $norm_street_name = preg_replace("/^([0-9]+) (.*) (N|S|E|W|NW|SW|SE)$/",
                                      "\\1 \\3 \\2",
                                      $norm_street_name);
-    $norm_street_name = preg_replace("/\\b(P\\.O\\. )?BOX\\b ?#?/", "",
+    $norm_street_name = preg_replace("/\\b(P\\.?O\\.? )?BOX\\b ?#?/", "",
                                      $norm_street_name);
     $norm_street_name = preg_replace("/ (STREET|ST|PLACE|PL|PLACE|AVE|AVENUE|DRIVE|DR|COURT|CT|EXPRESSWAY|EXPY|PARKWAY|PKWY|TURNPIKE|TPKE|BLVD|BOULEVARD|BOLEVARD|CIR|CIRCLE|LN|LANE|RD|ROAD|TER|TERR|TERRACE|WAY)\\.?$/", "", $norm_street_name);
     $norm_street_name = preg_replace(array("/\\bTHIRD\\b/",
