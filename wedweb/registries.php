fce478779c706cca5743a08c67e8ccda74bb5f35
Matthew Sachs <matthewg@zevils.com>
2008-09-04 03:53:33 +0000

    Update registry links

--- wedweb/registries.php
+++ wedweb/registries.php
@@ -7,8 +7,7 @@ Elizabeth and Matthew have set up several charitable donation funds in honor of 
 
 <p>If you choose to book your travel through Orbitz, please consider using the following link.  By following this link to access Orbitz when you are ready to book your travel, Orbitz will contribute to the funds listed above.  <a href="http://click.linksynergy.com/fs-bin/click?offerid=66478.10000071&type=4&subid=0&id=jYqsEyHsgkU&u1=29450">Access Orbitz with this link to donate.</a></p>
 <p>In lieu of a traditional wedding gift, a direct donation can be made through Elizabeth and Matthew's charity registry.  Please <a href="http://www.idofoundation.org/cgi-bin/viewdonations.cgi?WeddingID=29450">click here</a> to read about the work of the organizations listed above or to make a donation in honor of the wedding.</p>
-<p>Crate and Barrel has also teamed up with the I Do Foundation to make charitable contributions.  If you choose to purchase a gift through Elizabeth and Matthew&rsquo;s registry with <a href="http://registry.weddingchannel.com/registryJump.action?retailer_registry_uid=305823015&c=574007725&s=105&t=101&p=24&l=109306">Crate and Barrel</a>, a small percentage will be donated to one of their chosen charities.</p>
-<p>The couple has also registered at <a href="http://www.bedbathandbeyond.com/regGiftRegistry.asp?order_num=-1&wrn=%2D2025801719">Bed, Bath and Beyond</a>.</p>
+<p><a href="http://www.crateandbarrel.com/Gift-Registry/Guest/ViewRegistry.aspx?grid=3919188">Crate and Barrel</a> and <a href="http://registry.weddingchannel.com/registryJump.action?retailer_registry_uid=306467534&c=574007725&s=105&t=101&p=24&l=196431">Bed, Bath and Beyond</a> have also teamed up with the I Do Foundation to make charitable contributions.  If you choose to purchase a gift through Elizabeth and Matthew&rsquo;s registry with either of those stores, a small percentage will be donated to one of their chosen charities.</p>
 </ul>
 </p>
 <? include("footer.inc"); ?>
